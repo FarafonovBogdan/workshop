11438a3cf140dfc6afd7bf1b6c38e5e389d61d4f
Bogdan Farafonov <bogdanf.dev@gmail.com>
2021-03-19 18:20:50 +0200

    add config

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -4,19 +4,28 @@ import (
 	"log"
 	"net/http"
 
+	"workshop/internal/config"
 	"workshop/internal/handler"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 func main() {
+
+	cfg := config.Server{}
+	err := cleanenv.ReadConfig("config.yml", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 	h := handler.NewHandler()
+
 	r := chi.NewRouter()
 
 	r.Get("/hello", h.Hello)
 
 	log.Print("Starting server")
-	err := http.ListenAndServe(":8080", r)
+	err = http.ListenAndServe(":8080", r)
 	log.Fatal(err)
 
 	log.Print("Shutting server down")
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module workshop
 
 go 1.15
 
-require github.com/go-chi/chi/v5 v5.0.1
+require (
+	github.com/go-chi/chi/v5 v5.0.1
+	github.com/ilyakaznacheev/cleanenv v1.2.5
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
